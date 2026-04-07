c290ddd6142593802e94a1d681431af9b97a159b
Gourouvin Laurent <gourouvin.laurent@gmail.com>
2026-04-07 18:48:35 +0200

    feat: add findByAgentAndStatus in ConversationRepo

--- backend/src/main/java/com/yourcaryourway/poc/repository/ConversationRepository.java
+++ backend/src/main/java/com/yourcaryourway/poc/repository/ConversationRepository.java
@@ -1,14 +1,18 @@
 package com.yourcaryourway.poc.repository;
 
+import aj.org.objectweb.asm.commons.Remapper;
 import com.yourcaryourway.poc.model.Conversation;
 import com.yourcaryourway.poc.model.ConversationStatus;
+import com.yourcaryourway.poc.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ConversationRepository extends JpaRepository<Conversation, UUID> {
     List<Conversation> findByStatus(ConversationStatus status);
+    List<Conversation> findByAgentAndStatus(User agent, ConversationStatus status);
 }

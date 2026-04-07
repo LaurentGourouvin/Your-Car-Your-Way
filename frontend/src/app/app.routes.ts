7d9ea05123c5f765271758a4fbd9cc045a1f2147
Gourouvin Laurent <gourouvin.laurent@gmail.com>
2026-04-07 17:38:42 +0200

    feat(router): update router

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login/login.component').then((r) => r.LoginComponent),
+  },
+  /*{
+    path: 'chat',
+    loadComponent: () =>
+      import('./features/chat/conversation/conversation.component').then(
+        (r) => r.ConversationComponent,
+      ),
+  },
+  {
+    path: 'queue',
+    loadComponent: () =>
+      import('./features/chat/queue/queue.component').then((r) => r.QueueComponent),
+  },*/
+];
